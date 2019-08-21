2a16d93a5020401a25485cf8a45e4e4ba96d0afa
Carsten Limberger <carsten.limberger@gmx.net>
2019-08-21 19:52:51 +0200

    Improve creation process for sqlite database table

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import mysql.connector
 
 conn = sqlite3.connect('quotes.db')
 curr = conn.cursor()
-curr.execute('CREATE TABLE quotes(text text, author text, tags text)')
+curr.execute('CREATE TABLE IF NOT EXISTS quotes(text text, author text, tags text)')
 curr.execute('INSERT INTO quotes VALUES ("Any text", "Fritz Mueller", "any_tag")')
 conn.commit()
 conn.close()
